5dbd3a1629c548db6a571291358e1d748d188128
jinsuichen <ischenjinsui@gmail.com>
2023-02-11 16:09:37 +0800

    feat: change colors of buttons at header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 
 import './Header.scss'
-import { Button } from 'antd';
+import {Button, ConfigProvider} from 'antd';
 
 class Header extends Component {
     render() {
         return (
             <div className={'Header'}>
-                <Button type="primary" shape="circle" danger/>
-                <Button type="primary" shape="circle" />
+                <ConfigProvider theme={{ token: { colorPrimary: '#ff4d4f'} }}>
+                    <Button type="primary" shape="circle" />
+                </ConfigProvider>
+                <ConfigProvider theme={{ token: { colorPrimary: '#9B9B9B'} }}>
+                    <Button type="primary" shape="circle" />
+                </ConfigProvider>
+
             </div>
         );
     }
