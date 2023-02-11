a8930211bc7ad85cfc4891bb6068d267751f73c3
jinsuichen <ischenjinsui@gmail.com>
2023-02-11 19:19:18 +0800

    feat: complete component CustomizationButton

---
+++ src/components/CustomizationButton/CustomizationButton.tsx
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import {Button, ConfigProvider} from "antd";
+
+import './CustomizationButton.scss'
+
+type Props = {
+    customizationType: 'tiny' | 'normal' | 'icon',
+    color?: string,
+    shape?: "round" | "circle" | "default",
+    size?: "small" | "middle" | "large",
+    icon?: React.ReactNode,
+    children?: any,
+    danger?: boolean,
+    className?: string,
+    onClick?: (React.MouseEventHandler<HTMLAnchorElement> & React.MouseEventHandler<HTMLButtonElement>) | undefined;
+}
+
+class CustomizationButton extends Component<Props> {
+
+    static defaultProps: Partial<Props> = {
+        color: 'white',
+        shape: 'default',
+        size: 'small',
+        icon: undefined,
+        children: undefined,
+        danger: false,
+    }
+    render() {
+        const {color, customizationType, icon, children, size, shape, danger, className} = this.props;
+        return (
+            <ConfigProvider theme={{ token: { colorPrimary: color} }}>
+                <Button
+                    {...this.props}
+                    className={customizationType + 'Button ' + className}
+                    type="primary"
+                />
+            </ConfigProvider>
+        );
+    }
+}
+
+export default CustomizationButton;
