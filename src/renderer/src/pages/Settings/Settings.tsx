9149a1fdbea07a0a108e0ee045c071d49cae559c
jinsuichen <ischenjinsui@gmail.com>
2023-02-12 12:06:32 +0800

    feat: Change the color of the pagination numbers on the settings page from blue(default) to dark green

--- src/renderer/src/pages/Settings/Settings.tsx
+++ src/renderer/src/pages/Settings/Settings.tsx
@@ -1,5 +1,5 @@
 import { Component } from 'react'
-import { List } from 'antd'
+import { ConfigProvider, List } from 'antd'
 
 import CustomizationButton from '../../components/CustomizationButton/CustomizationButton'
 const { api } = window
@@ -37,26 +37,28 @@ class Settings extends Component<Record<string, never>, State> {
 
     return (
       <div className={'Settings'}>
-        <List
-          size={'small'}
-          bordered
-          pagination={{
-            align: 'center',
-            pageSize: 6
-          }}
-          dataSource={pathList}
-          renderItem={(item): JSX.Element => (
-            <List.Item className={'ListItem'}>
-              <p className={'TextField'}>{item}</p>
-              <CustomizationButton
-                className={'InlineButton'}
-                type={'tiny'}
-                danger
-              ></CustomizationButton>
-              {/*<Button danger className={'InlineButton'} type="primary" size={'small'}>delete</Button>*/}
-            </List.Item>
-          )}
-        />
+        <ConfigProvider theme={{ token: { colorPrimary: '#407a52' } }}>
+          <List
+            size={'small'}
+            bordered
+            pagination={{
+              align: 'center',
+              pageSize: 6
+            }}
+            dataSource={pathList}
+            renderItem={(item): JSX.Element => (
+              <List.Item className={'ListItem'}>
+                <p className={'TextField'}>{item}</p>
+                <CustomizationButton
+                  className={'InlineButton'}
+                  type={'tiny'}
+                  danger
+                ></CustomizationButton>
+                {/*<Button danger className={'InlineButton'} type="primary" size={'small'}>delete</Button>*/}
+              </List.Item>
+            )}
+          />
+        </ConfigProvider>
         <CustomizationButton
           className={'AddButton'}
           color={'#407A52'}
